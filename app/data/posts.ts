4e9a9d25789fca31e3d65d472b5be5aa54bf8100
berkaycelik1 <cberkay959@gmail.com>
2026-01-07 18:12:19 +0300

    Initial Next.js blog setup

--- app/data/posts.ts
+++ app/data/posts.ts
@@ -2,6 +2,6 @@ export const posts = [
   {
     id: 1,
     title: "Berkay Çelik",
-    description: "Merhaba ben Berkay! Bu benim ilk web sayfam🙋‍♂️"
+    description: "Merhaba ben Berkay! Bu benim ilk web sayfam."
   }
 ];
