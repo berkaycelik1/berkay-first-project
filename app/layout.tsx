5a56df79d50966ccb0bd7f00dfb1d54505847448
berkaycelik1 <cberkay959@gmail.com>
2026-02-04 18:10:07 +0300

    deneme

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </div>
         </nav>
 
-        {/* Ana İçerik - İşte burası her şeyi ortalayacak */}
+        
         <main className="container mx-auto max-w-5xl px-4 py-10">
           {children}
         </main>
