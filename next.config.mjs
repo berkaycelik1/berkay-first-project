4ff4524c9d542070e060be0a98ff4d55deec8c2e
atakan <94399983+benevolentsocialplanner@users.noreply.github.com>
2026-02-07 14:51:52 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["tailwindcss", "@tailwindcss/postcss"],
-};
+const nextConfig = {};
 
 export default nextConfig;
